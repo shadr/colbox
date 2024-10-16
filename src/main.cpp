698473c37ba8cea38a04eecb990a6cc72440912a
Nesterov Nikita <shadr.nn@gmail.com>
2024-10-16 16:13:27 +0300

    use quadtree to improve collision performance

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,18 @@
 #include <entt/entt.hpp>
 #include <glm/glm.hpp>
 #include <imgui.h>
+#include <iostream>
 #include <numbers>
 #include <random>
 #include <raylib.h>
+
+#include "Quadtree.h"
 
 auto rng =
     std::mt19937(std::chrono::steady_clock::now().time_since_epoch().count());
 
 std::uniform_real_distribution<float> dis_position(-300.0, 300.0);
-std::uniform_real_distribution<float> dis_size(4.0, 8.0);
+std::uniform_real_distribution<float> dis_size(2.0, 4.0);
 std::uniform_real_distribution<float> dis_vel_angle(0.0, 2 * std::numbers::pi);
 std::uniform_real_distribution<float> dis_start_color(0.0, 360.0);
 
@@ -25,6 +28,11 @@ struct PositionComponent {
             static_cast<float>(GetScreenWidth()) / 2 + dis_position(rng),
             static_cast<float>(GetScreenHeight()) / 2 + dis_position(rng))),
         radius(dis_size(rng)) {}
+
+  Rectangle rect() {
+    return Rectangle(pos.x - radius, pos.y - radius, 2.0f * radius,
+                     2.0f * radius);
+  }
 };
 
 struct VelocityComponent {
@@ -32,7 +40,8 @@ struct VelocityComponent {
 
   VelocityComponent()
       : vel(glm::vec2(std::cos(dis_vel_angle(rng)),
-                      std::sin(dis_vel_angle(rng)) * 150.)) {}
+                      std::sin(dis_vel_angle(rng))) *
+            150.0f) {}
 };
 
 struct ColorComponent {
@@ -91,27 +100,38 @@ void wall_collision_system(entt::registry &reg) {
 
 void circle_collision_system(entt::registry &reg) {
   const auto view = reg.view<PositionComponent, VelocityComponent>();
-  for (auto e1 : view) {
+  auto getBox = [](std::pair<Rectangle, entt::entity> pair) {
+    return quadtree::Box<float>(pair.first.x, pair.first.y, pair.first.width,
+                                pair.first.height);
+  };
+  quadtree::Quadtree tree =
+      quadtree::Quadtree<std::pair<Rectangle, entt::entity>, decltype(getBox)>(
+          quadtree::Box<float>(-16.0f, -16.0f, GetScreenWidth() + 32.0f,
+                               GetScreenHeight() + 32.0f));
+  for (auto e : view) {
+    auto &p = view.get<PositionComponent>(e);
+    tree.add(std::pair(p.rect(), e));
+  }
+  auto intersections = tree.findAllIntersections();
+  for (auto intersection : intersections) {
+    auto &e1 = intersection.first.second;
+    auto &e2 = intersection.second.second;
     auto &p1 = view.get<PositionComponent>(e1);
     auto &v1 = view.get<VelocityComponent>(e1);
-    for (auto e2 : view) {
-      if (e1 == e2) {
-        continue;
-      }
-      auto &p2 = view.get<PositionComponent>(e2);
-      auto &v2 = view.get<VelocityComponent>(e2);
-      float distance = glm::distance(p1.pos, p2.pos);
-      bool collide = distance <= p1.radius + p2.radius;
+    auto &p2 = view.get<PositionComponent>(e2);
+    auto &v2 = view.get<VelocityComponent>(e2);
 
-      if (collide) {
-        float overlap_length = distance - p1.radius - p2.radius;
-        glm::vec2 n = glm::normalize(p2.pos - p1.pos);
-        p1.pos += n * overlap_length / 2.0f;
-        p2.pos -= n * overlap_length / 2.0f;
-        float p = glm::dot(v1.vel, n) - glm::dot(v2.vel, n);
-        v1.vel = v1.vel - p * n;
-        v2.vel = v2.vel + p * n;
-      }
+    float distance = glm::distance(p1.pos, p2.pos);
+    bool collide = distance <= p1.radius + p2.radius;
+
+    if (collide) {
+      float overlap_length = distance - p1.radius - p2.radius;
+      glm::vec2 n = glm::normalize(p2.pos - p1.pos);
+      p1.pos += n * overlap_length / 2.0f;
+      p2.pos -= n * overlap_length / 2.0f;
+      float p = glm::dot(v1.vel, n) - glm::dot(v2.vel, n);
+      v1.vel = v1.vel - p * n;
+      v2.vel = v2.vel + p * n;
     }
   }
 }
@@ -120,12 +140,13 @@ int main() {
   const int screenWidth = 1280;
   const int screenHeight = 720;
 
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(screenWidth, screenHeight, "raylibapp");
   // SetTargetFPS(60);
   rlImGuiSetup(true);
 
   entt::registry registry;
-  for (int i = 0; i < 500; i++) {
+  for (int i = 0; i < 50; i++) {
     const auto entity = registry.create();
     registry.emplace<PositionComponent>(entity);
     registry.emplace<VelocityComponent>(entity);
