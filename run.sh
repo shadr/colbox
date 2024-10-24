41bc8efaca24fb18f066a6fd7dee61dd6d70e3bf
Nesterov Nikita <shadr.nn@gmail.com>
2024-10-24 06:45:59 +0300

    remove old `run.sh` script

--- run.sh
+++
@@ -1,2 +0,0 @@
-# filters out glfw warning that indicates that wayland does not support getting window position
-time cmake --build . -j9 && ./main 2>&1  | rg --invert-match "WARNING: GLFW: Error: 65548"
