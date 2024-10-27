9b2ced4a896f0a917ad3a69a19a956deefd51d48
Nesterov Nikita <shadr.nn@gmail.com>
2024-10-27 16:57:41 +0300

    use emscripten's main loop to improve performance

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,15 @@
 #include "random_ranges.hpp"
 #include "raylib.h"
 
+#if __EMSCRIPTEN__
+#include "emscripten.h"
+
+void game_loop(void *arg) {
+  Game *game = static_cast<Game *>(arg);
+  game->loop();
+}
+#endif
+
 int main() {
   const int screenWidth = 1280;
   const int screenHeight = 720;
@@ -19,9 +28,13 @@ int main() {
 
   game.init_world();
 
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop_arg(game_loop, &game, 0, 1);
+#else
   while (!WindowShouldClose()) {
     game.loop();
   }
+#endif
 
   rlImGuiShutdown();
   CloseWindow();
